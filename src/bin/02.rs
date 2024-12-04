a689d3f0246ae0f3f118781c717eb004e6aed905
Mikołaj "Mitos" Mijakowski <fenix.warld@gmail.com>
2024-12-04 21:22:56 +0100

    AoC day 02 - part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -73,63 +73,74 @@ fn main() -> Result<()> {
     //region Part 2
     println!("\n=== Part 2 ===");
 
-    fn is_safe(report: &[i32], ignore: usize) -> bool {
-        let mut report = report.iter().enumerate();
-
-        // First pass
-        let (i, first) = report.next().unwrap();
-        let (j, mut last) = report.next().unwrap();
-        let dif = first - last;
-        let order = dif.signum();
-
-        if 3 < dif.abs() || dif.abs() < 1 {
-            return false;
-        }
-
-        // Check if report is safe
-        for level in report {
-            let dif = last - level;
-
-            // Check safety conditions
-            if order != dif.signum() || 1 > dif.abs() || dif.abs() > 3 {
-                if ignored {
-                    return false;
-                } else {
-                    ignored = true;
-                    continue;
-                }
-            }
-
-            last = level.clone();
-        }
-
-        // All checks has passed
-        return true;
-    }
-
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
         // How many reports are safe
         let mut answer: usize = 0;
+
+        fn is_safe(report: &[i32], ignore: usize) -> bool {
+            // Setup variables
+            let mut report = report.iter().enumerate();
+
+            // Ignore first level
+            if ignore == 0 {
+                report.next();
+            }
+
+            // Get first level
+            let (_i, &first) = report.next().unwrap();
+
+            // Ignore second level
+            if ignore == 1 {
+                report.next();
+            }
+
+            // Get second level
+            let (_j, &last) = report.next().unwrap();
+            let mut last = last;
+
+            // Calculate properties
+            let dif = first - last;
+            let ord = dif.signum();
+            let dif = dif.abs();
+
+            // First safety condition
+            if !(1 <= dif && dif <= 3) {
+                return false;
+            }
+
+            for (i, &level) in report {
+                if i == ignore {
+                    continue;
+                }
+
+                // Check safety conditions
+                let dif = last - level;
+                if ord != dif.signum() || !(1 <= dif.abs() && dif.abs() <= 3) {
+                    return false;
+                }
+                last = level;
+            }
+
+            // All check passed
+            return true;
+        }
+
+        assert!(is_safe(&vec![1, 3, 2, 4, 5], 2));
+        assert!(is_safe(&vec![1, 3, 6, 7, 9], 5));
 
         // Iterate over reports
         for report in reader.lines() {
             let report = report.unwrap();
             let report = report.split_whitespace();
             let report: Vec<i32> = report.map(|x| x.parse::<i32>().unwrap()).collect();
+            let mut safe = false;
 
-            if is_safe(&report, false) {
-                // Increment counter
+            for i in 0..report.len() + 1 {
+                safe = safe || is_safe(&report, i);
+            }
+
+            if safe {
                 answer += 1;
-            } else if is_safe(&report[1..], true) {
-                answer += 1;
-            } else {
-                let mut modified: Vec<i32> = vec![report[0]];
-                modified.append(&mut report[2..].iter().copied().collect());
-                if is_safe(&modified, true) {
-                    answer += 1;
-                } else {
-                    println!("{:?}", report);
-                }
             }
         }
 
@@ -138,7 +149,6 @@ fn main() -> Result<()> {
     }
 
     assert_eq!(4, part2(BufReader::new(TEST.as_bytes()))?);
-    println!("");
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part2(input_file)?);
