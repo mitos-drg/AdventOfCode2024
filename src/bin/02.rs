5d770571d13ce851709bca790b435aae4a62a5b7
Mikołaj "Mitos" Mijakowski <fenix.warld@gmail.com>
2024-12-04 20:43:29 +0100

    Temporary state

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -71,17 +71,78 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn is_safe(report: &[i32], ignore: usize) -> bool {
+        let mut report = report.iter().enumerate();
+
+        // First pass
+        let (i, first) = report.next().unwrap();
+        let (j, mut last) = report.next().unwrap();
+        let dif = first - last;
+        let order = dif.signum();
+
+        if 3 < dif.abs() || dif.abs() < 1 {
+            return false;
+        }
+
+        // Check if report is safe
+        for level in report {
+            let dif = last - level;
+
+            // Check safety conditions
+            if order != dif.signum() || 1 > dif.abs() || dif.abs() > 3 {
+                if ignored {
+                    return false;
+                } else {
+                    ignored = true;
+                    continue;
+                }
+            }
+
+            last = level.clone();
+        }
+
+        // All checks has passed
+        return true;
+    }
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        // How many reports are safe
+        let mut answer: usize = 0;
+
+        // Iterate over reports
+        for report in reader.lines() {
+            let report = report.unwrap();
+            let report = report.split_whitespace();
+            let report: Vec<i32> = report.map(|x| x.parse::<i32>().unwrap()).collect();
+
+            if is_safe(&report, false) {
+                // Increment counter
+                answer += 1;
+            } else if is_safe(&report[1..], true) {
+                answer += 1;
+            } else {
+                let mut modified: Vec<i32> = vec![report[0]];
+                modified.append(&mut report[2..].iter().copied().collect());
+                if is_safe(&modified, true) {
+                    answer += 1;
+                } else {
+                    println!("{:?}", report);
+                }
+            }
+        }
+
+        // Return answer
+        Ok(answer)
+    }
+
+    assert_eq!(4, part2(BufReader::new(TEST.as_bytes()))?);
+    println!("");
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
